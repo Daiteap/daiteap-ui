b47fb50202b5427f5c0ec3e27e1343b2f2f0d634
Desislav Arashev <darashev@cst-bg.net>
2023-01-19 14:43:34 +0200

    Fix Azure OAuth

--- app/src/components/platform/oauth/AzureCreateApp.vue
+++ app/src/components/platform/oauth/AzureCreateApp.vue
@@ -30,6 +30,7 @@ export default {
       msg: "",
       error: "",
       error_description: "",
+      subscriptionId: "",
       loading: true
     };
   },
@@ -49,7 +50,7 @@ export default {
       let splittedState = self.$route.query.authState.split("|");
       let tenant = splittedState[0];
       self.msg = "Getting azure credentials...";
-      let subscriptionId = splittedState[1];
+      self.subscriptionId = splittedState[1];
 
       let authCode = self.$route.query.authCode;
       this.axios
@@ -57,7 +58,7 @@ export default {
           "/server/oauth/azure/createApp",
           {
             tenant: tenant,
-            subscriptionId: subscriptionId,
+            subscriptionId: self.subscriptionId,
             authCode: authCode,
             origin: window.origin
           },
@@ -111,13 +112,33 @@ export default {
               self.error = response.data.errorMessage;
             }
             else {
-              self.$notify({
-                group: "msg",
-                type: "success",
-                title: "Notification:",
-                text: "Successfuly created OAuth credentials"
-              });
-              self.backToCloudProfile()
+              console.log(response.data);
+              self.axios
+                .post(
+                  "/server/oauth/azure/getgrantauthurladminconsent",
+                  { origin: window.origin, subscriptionId: self.subscriptionId },
+                  self.get_axiosConfig()
+                )
+                .then(function (response) {
+                  console.log(response.data);
+                  window.location.href = response.data.auth_url;
+                })
+                .catch(function (error) {
+                  self.loading = false;
+                  console.log(error);
+                  self.error = error;
+                  if (error.response) {
+                    console.log(error.response.data);
+                    self.error_description = error.response.data;
+                  }
+
+                  self.$notify({
+                    group: "msg",
+                    type: "error",
+                    title: "Notification:",
+                    text: "Error creating user credentials"
+                  });
+                });
             }
           }
         })
@@ -132,9 +153,6 @@ export default {
           console.log(error);
         });
     },
-    backToCloudProfile() {
-      this.$router.push({ name: "CloudProfile" });
-    }
   },
   destroyed() {
     if (window.intervals) {
