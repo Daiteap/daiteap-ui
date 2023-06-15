2151b9806d4125d029f11374619e0ecfae76cea1
Desislav Arashev <darashev@cst-bg.net>
2023-06-15 17:21:29 +0300

    Add tests for computeoverviewtable

---
+++ app/tests/unit/table/ComputeOverviewTable.spec.js
@@ -0,0 +1,127 @@
+import {shallowMount} from "@vue/test-utils";
+import ComputeOverviewTable from
+  "@/components/platform/tables/ComputeOverviewTable.vue";
+
+describe("ComputeOverviewTable", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(ComputeOverviewTable, {
+      propsData: {
+        projectName: "",
+      },
+      data() {
+        return {
+          clustersList: [
+            {
+              ID: 1,
+              Name: "Cluster 1",
+              Description: "This is the first cluster",
+              ProjectName: "Project A",
+              Providers: ["Azure", "Amazon"],
+              Credentials: {
+                azure: "azure-credentials",
+                aws: "aws-credentials",
+              },
+              CreatedAt: "2021-08-01T12:00:00Z",
+              Contact: "John Doe",
+              Status: "running",
+              InstallStep: 0,
+              ResizeStep: 0,
+            },
+            {
+              ID: 2,
+              Name: "Cluster 2",
+              Description: "This is the second cluster",
+              ProjectName: "Project B",
+              Providers: ["Google", "Onpremise"],
+              Credentials: {
+                google: "google-credentials",
+                onpremise: "onpremise-credentials",
+              },
+              CreatedAt: "2021-08-02T12:00:00Z",
+              Contact: "Jane Smith",
+              Status: "starting",
+              InstallStep: 50,
+              ResizeStep: 0,
+            },
+            {
+              ID: 3,
+              Name: "Cluster 3",
+              Description: "This is the third cluster",
+              ProjectName: "Project C",
+              Providers: ["Openstack", "IotArm"],
+              Credentials: {
+                openstack: "openstack-credentials",
+                iotarm: "iotarm-credentials",
+              },
+              CreatedAt: "2021-08-03T12:00:00Z",
+              Contact: "Bob Johnson",
+              Status: "stopping",
+              InstallStep: 100,
+              ResizeStep: 0,
+            },
+          ],
+          loading: false,
+          computed_account_settings: {
+            enable_templates: false,
+          },
+        };
+      },
+    });
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("renders the component", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("displays the clusters list", async () => {
+    await wrapper.vm.$nextTick();
+
+    const cluster1Name = wrapper.findAll("td").at(0).text();
+    const cluster1Description = wrapper.findAll("td").at(1).text();
+    const cluster1Project = wrapper.findAll("td").at(2).text();
+    const cluster1CreatedAt = wrapper.findAll("td").at(4).text();
+    const cluster1CreatedBy = wrapper.findAll("td").at(5).text();
+    const cluster2Name = wrapper.findAll("td").at(8).text();
+    const cluster2Description = wrapper.findAll("td").at(9).text();
+    const cluster2Project = wrapper.findAll("td").at(10).text();
+    const cluster2CreatedAt = wrapper.findAll("td").at(12).text();
+    const cluster2CreatedBy = wrapper.findAll("td").at(13).text();
+
+    expect(cluster1Name).toBe("Cluster 1");
+    expect(cluster1Description).toBe("This is the first cluster");
+    expect(cluster1Project).toBe("Project A");
+    expect(cluster1CreatedAt).toBe("2021-08-01T12:00:00Z");
+    expect(cluster1CreatedBy).toBe("John Doe");
+    expect(cluster2Name).toBe("Cluster 2");
+    expect(cluster2Description).toBe("This is the second cluster");
+    expect(cluster2Project).toBe("Project B");
+    expect(cluster2CreatedAt).toBe("2021-08-02T12:00:00Z");
+    expect(cluster2CreatedBy).toBe("Jane Smith");
+  });
+
+  it("displays a loading spinner when the table is loading", async () => {
+    wrapper.setData({loading: true});
+
+    await wrapper.vm.$nextTick();
+
+    const spinner = wrapper.find(".spinner-border");
+
+    expect(spinner.exists()).toBe(true);
+  });
+
+  it("displays a message when there are no clusters", async () => {
+    wrapper.setData({clustersList: []});
+
+    await wrapper.vm.$nextTick();
+
+    const message = wrapper.findAll("div").at(0);
+
+    expect(message.text()).toBe("No Compute (VMs) currently.");
+  });
+});
